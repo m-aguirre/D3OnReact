ce1af4f6cf9ec40690b4595305ffdb44d3f79db8
m-aguirre <mpaguirre@dons.usfca.edu>
2018-01-21 11:56:54 -0800

    BB renders axes

--- src/BollingerBands.js
+++ src/BollingerBands.js
@@ -4,8 +4,68 @@ import DateScale from "./DateScale.js"
 
 class BollingerBands {
   constructor(data, currentDate, daysToSubtract) {
-    
+    this.data = data;
+    this.dataSummary = {
+      minClosingValue: d3.min(this.data, (d) => {return d.close}),
+      maxClosingValue: d3.max(this.data, (d) => {return d.close}),
+      mean: 0,
+      sd: 0,
+      intercept: 0,
+      regressionCoef: 0
+    }
+
+    this.daysToSubtract = daysToSubtract;
+    //controls speed of animation
+    this.delayFactor = 8;
+    this.endDate = currentDate;
+
+    this.xcoord = new DateScale(currentDate, daysToSubtract);
+    this.xScale = this.xcoord.xScale;
+
+    //creates y scale based on min and max closing prices
+    this.yScale = d3.scaleLinear()
+        .domain([this.maxYdomain(),this.minYdomain()])
+        .range([0,450]);
+    this.xAxis = d3.axisBottom(this.xScale).ticks(this.xcoord.numTicks);
+    this.yAxis = d3.axisLeft(this.yScale).ticks(6);
+
+
+    this.addViewport()
+
   }
+  maxYdomain() {
+    return parseInt(this.dataSummary.maxClosingValue) + (parseInt(this.dataSummary.maxClosingValue)/5.0);
+  }
+
+  minYdomain() {
+    return parseInt(this.dataSummary.minClosingValue) - (parseInt(this.dataSummary.minClosingValue)/5.0);
+  }
+
+  addViewport() {
+    d3.select('.graph-pane')
+      .append('svg')
+      .attr('class', 'viewport')
+      .attr('width', 700)
+      .attr('height', 450)
+
+    this.placeXAxis();
+    this.placeYAxis();
+  }
+
+  placeXAxis() {
+    d3.select('.viewport')
+    .append('g')
+    .attr('transform', 'translate(0,' + (450) + ')') //putting it at 'height' (== 250) pushes scale off the graph
+    .call(this.xAxis)
+  }
+
+  placeYAxis() {
+    d3.select('.viewport')
+    .append('g')
+    .attr('transform', 'translate(0,' + 0 + ')')
+    .call(this.yAxis)
+  }
+
 }
 
 export default BollingerBands;
